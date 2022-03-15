b8b49e3d686db890e4a791186deb37d77a39ed41
yadavan <yadavan100@hotmail.com>
2022-03-15 11:12:11 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React,{ useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+ 
+  const APP_ID = "ae5262b5";
+  const APP_KEY = "233fdb8a6915262b201daa19a552dfa6";
+
+ 
+
+  
+
+  useEffect( () =>{ 
+    getRecipes();
+  },[]);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json();
+    console.log(data);
+  }
+
+ return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <form className="search-form">
+        <input className="search-bar" type = "text"/>
+        <button className="search-button" type = "submit"> Search</button>
+      </form>
     </div>
   );
-}
+
+};
+  
+
+
 
 export default App;
+
+
+
+
