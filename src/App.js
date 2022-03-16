5558081a8a5174f199219f5411f5ea5a51eb176f
yadavan <yadavan100@hotmail.com>
2022-03-16 13:47:41 -0400

    Created recipe component and updated search input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React,{ useState, useEffect } from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
 const App = () => {
@@ -6,7 +7,10 @@ const App = () => {
   const APP_ID = "ae5262b5";
   const APP_KEY = "233fdb8a6915262b201daa19a552dfa6";
 
- 
+  const[recipes,setRecipes] = useState([]);
+  const[search,setSearch] = useState('');
+  const[query,setQuery] = useState('chicken');
+
 
   
 
@@ -17,16 +21,34 @@ const App = () => {
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
     const data = await response.json();
-    console.log(data);
-  }
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+
+  
 
  return(
     <div className="App">
      
       <form className="search-form">
-        <input className="search-bar" type = "text"/>
+       <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search-button" type = "submit"> Search</button>
       </form>
+     {recipes.map(recipe =>(
+       <Recipe 
+       key={recipe.recipe.label}
+       title={recipe.recipe.label} 
+       calories={recipe.recipe.calories}
+       image={recipe.recipe.image}
+
+       />
+
+     ))}
     </div>
   );
 
